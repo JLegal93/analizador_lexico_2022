493b02d36ebecd76276afd5c0b03dc88d559f498
Junior Legal <jlegal@outlook.com>
2022-04-11 02:18:05 -0400

    lexer fx

--- analizador-lexico.py
+++ analizador-lexico.py
@@ -5,12 +5,13 @@ output_file = open("output_file.txt", "w")
 def getToken():
     global currentToken
     global currentIndex
-    if(currentIndex <= len(fileText)):
+    if(currentIndex < len(fileText)-1):
         currentToken = fileText[currentIndex]
         currentIndex += 1
-        print(currentIndex)
+        return True
     else:
-        return 'eof'
+        currentToken = ''
+        return False
 
 
 def tmatch (spectedToken):
@@ -21,7 +22,7 @@ def tmatch (spectedToken):
 def stringrp():
     if (currentToken == '"'):
         tmatch('"')
-        output_file.write('STRING')
+        output_file.write('STRING ')
     else:
         getToken()
         stringrp()
@@ -34,16 +35,93 @@ def stringr():
     else:
         stringrp()
 
+def list():
+    if (currentToken == '['):
+        tmatch('[')
+        output_file.write('L_CORCHETE \n\t')
+        attr()
+    elif (currentToken == ','):
+        attr()
+    elif (currentToken == ']'):
+        tmatch(']')
+        output_file.write('L_CORCHETE\n\t')
+        attr()
+
+def truer():
+    if (currentToken == 't'):
+        trueString = currentToken
+        for number in range(3):
+            getToken()
+            trueString += currentToken
+        if (trueString.lower() == 'true'):
+            output_file.write('PR_TRUE ')
+            attr()
+
+def falser():
+    if (currentToken == 'f'):
+        trueString = currentToken
+        for number in range(4):
+            getToken()
+            trueString += currentToken
+        if (trueString.lower() == 'false'):
+            output_file.write('PR_FALSE ')
+            attr()
+
+def numberr():
+    if (currentToken.isnumeric()):
+        getToken()
+        numberr()
+    else:
+        attr()
+
+def attr():
+    if (currentToken == '"'):
+        stringr()
+        attr()
+    elif (currentToken == ':'):
+        tmatch(':')
+        output_file.write('DOS_PUNTOS ')
+        attr()
+    elif (currentToken == '{'):
+        obj()
+    elif (currentToken == '['):
+        list()
+    elif (currentToken == ']'):
+        tmatch(']')
+        output_file.write('R_CORCHETE \n')
+        attr()
+    elif (currentToken == 't'):
+        truer()
+    elif (currentToken == 'f'):
+        falser()
+    elif (currentToken.isnumeric()):
+        output_file.write('NUMBER ')
+        getToken()
+        numberr()
+    elif (currentToken == '}'):
+        obj()
+    elif (currentToken == ','):
+        tmatch(',')
+        output_file.write('COMA \n\t')
+        attr()
+    elif(getToken()):
+        attr()
+
+
 def obj():
+    if (currentToken == '{'):
         tmatch('{')
-        output_file.write('L_LLAVE')
-        #stringr()
+        output_file.write('L_LLAVE \n\t')
+    if (currentToken == '}'):
+        tmatch('}')
+        output_file.write('R_LLAVE\n')
+    attr()
+
+
 
 def json():
     getToken()
-    if(currentToken == '{'):
-        obj()
-
+    obj()
     output_file.close()
 
 
